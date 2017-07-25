8d0087b57686e0115cc564ef922c30d9afa0d115
Carson Canales, David Cuthell and Evan Johnson <evan@>
2017-07-25 10:06:00 -0700

    create add panel function

---
+++ README.md
@@ -0,0 +1,27 @@
+# eCommerceSite
+
+####  Play pig dice against another user or the computer!
+
+#### By Carson Canales, Evan Johnson, Haneen AbuKhater, David Cuthell, Kate Byars
+
+## Description
+_Allow shoppers to order products from a local exotic produce market_
+* HTML
+* CSS
+* Bootstrap
+* JQuery
+* JavaScript
+
+## Development Specifications
+| Behavior      | Example Input         | Example Output        |
+| ------------- | ------------- | ------------- |
+| This program will match a username and a passcode | Bob Dole ******  |  Bob Dole's Account  |
+| The program will add a product to checkout             |  Apple   | Apple    |
+| The program will add a product quantity to checkout | 3    |    3      |
+| The program will add the total prices of the products | 3  |  Total = $20 |
+
+## Setup/Installation Requirements
+_Click link below_
+
+### License
+MIT Copyright (c) 2017 **_Carson Canels && Evan Johnson && Haneen AbuKhater && David Cuthell && Kate Byars_**
--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,47 @@ NewAccount.prototype.fullName = function() {
 
 //User Interface
 $(document).ready(function() {
-
   var accountBank = []; //Hold accounts in array
+
+  function showProducts(productArray) {
+    var colCount = 3;
+    var output = "";
+
+    for (var i =0; i < productArray.length; i++) {
+      if(colCount === 3){
+        output += '<div class="row">' //start a new row when 3 columns
+      }
+      output += '<div class ="col-md-4">' +
+                  '<div class="panel panel-default">' +
+                    '<div class="panel-heading">' +
+                      '<p class="style1">' +
+                      productArray[i].goodName+
+                      '</p>'+
+                    '</div>'+
+                    '<div class="panel-body">'+
+                      '<p>Age: ' + productArray[i].age +', species: ' + productArray[i].species + '</p>' +
+                      '<div class="checkbox">' +
+                        '<label><input type="checkbox" value="" id="' + productArray[i].idNum + '">Adopt Me!</label>'+
+                      '</div>'+
+                    '</div>'+
+                  '</div>'+
+                '</div>';
+      colCount--;
+      if (colCount === 0) {
+        output += '</div>'; //When three columns, add closing div for row
+        colCount = 3;
+      }
+    }
+    if (productArray.length%3 !== 0) {
+      output += '</div>';
+    }
+    return output;
+  }
+
+
+
+
+
   $('#newAccount').submit(function(e) {
     e.preventDefault();
     //Take values
@@ -42,6 +81,7 @@ $(document).ready(function() {
     alert('Welcome back, ' + accountHolder.fullName() );
     $('.form-group input').val(''); //Reset form fields
     $(".col-md-6").hide();
+    showProducts();
 
   });
 
